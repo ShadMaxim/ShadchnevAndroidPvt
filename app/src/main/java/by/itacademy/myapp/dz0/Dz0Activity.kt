bf0000cc40eb362163cca2c81d62ed71199e01cd
Maxim Shadchnev <shad88max@gmail.com>
2019-06-18 14:46:05 +0300

    устранил замечания по dz0

--- app/src/main/java/by/itacademy/myapp/dz0/Dz0Activity.kt
+++ app/src/main/java/by/itacademy/myapp/dz0/Dz0Activity.kt
@@ -2,65 +2,61 @@ package by.itacademy.myapp.dz0
 
 import android.app.Activity
 import android.graphics.drawable.Drawable
-import android.os.Build
-import android.support.annotation.RequiresApi
 import android.widget.Button
 import android.widget.TextView
-
 import android.os.Bundle
 import android.view.View
 import by.itacademy.myapp.R
 
 class Dz0Activity : Activity() {
 
-    private lateinit var tv1: TextView
-    private lateinit var tv2: TextView
-    private lateinit var button: Button
+    private lateinit var oneTextView: TextView
+    private lateinit var twoTextView: TextView
+    private lateinit var oneButton: Button
 
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dzo)
 
-        tv1 = findViewById<TextView>(R.id.tv1)
-        tv2 = findViewById<TextView>(R.id.tv2)
-        button = findViewById<Button>(R.id.button)
-
-        fun changeText() {
-            button.text = tv1.text
-            tv1.text = tv2.text
-            tv2.text = button.text
-            button.setText(R.string.button1)
-        }
-
-        fun changeBack() {
-            var back: Drawable = tv1.background
-            tv1.setBackgroundDrawable(tv2.background)
-            tv2.setBackgroundDrawable(back)
-        }
+        oneTextView = findViewById<TextView>(R.id.oneTextView)
+        twoTextView = findViewById<TextView>(R.id.twoTextView)
+        oneButton = findViewById<Button>(R.id.oneButton)
 
         val onClickListener = View.OnClickListener { v ->
             when (v.id) {
-                R.id.tv1 -> {
+                R.id.oneTextView -> {
                     changeText()
                     changeBack()
                 }
-                R.id.tv2 -> {
+                R.id.twoTextView -> {
                     changeText()
                     changeBack()
                 }
-                R.id.button -> {
+                R.id.oneButton -> {
                     changeText()
                     changeBack()
                 }
             }
         }
-        var q: Drawable = tv1.background
-        tv1.setBackgroundDrawable(tv2.background)
-        tv2.setBackgroundDrawable(q)
+        var temp: Drawable = oneTextView.background
+        oneTextView.setBackgroundDrawable(twoTextView.background)
+        twoTextView.setBackgroundDrawable(temp)
 
-        tv1.setOnClickListener(onClickListener)
-        tv2.setOnClickListener(onClickListener)
-        button.setOnClickListener(onClickListener)
+        oneTextView.setOnClickListener(onClickListener)
+        twoTextView.setOnClickListener(onClickListener)
+        oneButton.setOnClickListener(onClickListener)
+    }
+
+    private fun changeText() {
+        oneButton.text = oneTextView.text
+        oneTextView.text = twoTextView.text
+        twoTextView.text = oneButton.text
+        oneButton.setText(R.string.button1)
+    }
+
+    private fun changeBack() {
+        var back: Drawable = oneTextView.background
+        oneTextView.setBackgroundDrawable(twoTextView.background)
+        twoTextView.setBackgroundDrawable(back)
     }
 }
