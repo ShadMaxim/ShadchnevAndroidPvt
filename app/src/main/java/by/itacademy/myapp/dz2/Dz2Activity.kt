e2f42789cc32d78083d79775f6166bed3fdf3458
Maxim Shadchnev <shad88max@gmail.com>
2019-06-21 00:18:50 +0300

    сделал dz2, осталось только подобрать чвета

--- app/src/main/java/by/itacademy/myapp/dz2/Dz2Activity.kt
+++ app/src/main/java/by/itacademy/myapp/dz2/Dz2Activity.kt
@@ -5,31 +5,54 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import by.itacademy.myapp.R
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_dz2.*
+import java.lang.Exception
 
 class Dz2Activity : Activity() {
 
-    private lateinit var progressBar: ProgressBar
+    private lateinit var view: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz2)
 
-        progressBar = findViewById(R.id.progressBarDz2)
+        view = progressBarDz2
 
         val OnClickListener = View.OnClickListener { v ->
             when (v.id) {
                 R.id.loadButton -> {
-                    Picasso.get().load(uriEditText.text.toString())
-                        .placeholder(R.drawable.ic_login_password)
-                        .transform(CropCircleTransformation())
-                        .error(R.drawable.ic_login_name)
-                        .into(showImageView)
+                    showProgressBar()
+                    workPicasso()
                 }
             }
         }
         loadButton.setOnClickListener(OnClickListener)
+    }
+
+    private fun showProgressBar() {
+        view.visibility = View.VISIBLE
+    }
+
+    private fun unShowProgressBar() {
+        view.visibility = View.GONE
+    }
+
+    private fun workPicasso() {
+        Picasso.get()
+            .load(uriEditText.text.toString())
+            .transform(CropCircleTransformation())
+            .error(R.drawable.ic_close_black_24dp)
+            .into(showImageView, object : Callback {
+                override fun onSuccess() {
+                    unShowProgressBar()
+                }
+
+                override fun onError(e: Exception?) {
+                    unShowProgressBar()
+                }
+            })
     }
 }
--- app/src/test/java/by/itacademy/myapp/ExampleUnitTest.kt
+++ app/src/test/java/by/itacademy/myapp/ExampleUnitTest.kt
@@ -1,11 +1,11 @@
-/*ackage by.itacademy.myapp
+package by.itacademy.myapp
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
-*
+
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-}*/
+}
