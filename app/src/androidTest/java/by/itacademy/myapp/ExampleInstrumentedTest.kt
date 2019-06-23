4f9fc696915d712b7dc402541553562b89d4a091
Maxim Shadchnev <shad88max@gmail.com>
2019-06-23 14:16:28 +0300

    поменял размер ImageView что в режиме landscape нормально выглядело

--- app/src/androidTest/java/by/itacademy/myapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/by/itacademy/myapp/ExampleInstrumentedTest.kt
@@ -2,17 +2,10 @@ package by.itacademy.myapp
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
