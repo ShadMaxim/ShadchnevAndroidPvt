a876cf89eeb5376ecd4af5d2dd1a50899fe5449c
Maxim Shadchnev <shad88max@gmail.com>
2019-06-23 17:41:04 +0300

    создал ветку dz3

---
+++ app/src/main/java/by/itacademy/myapp/dz3/Dz3Activity.kt
@@ -0,0 +1,12 @@
+package by.itacademy.myapp.dz3
+
+import android.app.Activity
+import android.os.Bundle
+import by.itacademy.myapp.R
+
+class Dz3Activity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz3)
+    }
+}
