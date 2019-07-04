cc4fae85c6fc2f8cbc7c8ade0ad464e211b9665b
Maxim Shadchnev <shad88max@gmail.com>
2019-07-04 16:16:50 +0300

    добавил в манифест

--- app/src/main/java/by/itacademy/myapp/AllDzActivity.kt
+++ app/src/main/java/by/itacademy/myapp/AllDzActivity.kt
@@ -9,6 +9,7 @@ import by.itacademy.myapp.dz1.Dz1Activity
 import by.itacademy.myapp.dz2.Dz2Activity
 import by.itacademy.myapp.dz2.Dz2LoginActivity
 import by.itacademy.myapp.dz3.Dz3Activity
+import by.itacademy.myapp.dz6.Dz6StudentListActivity
 import kotlinx.android.synthetic.main.activity_alldz.*
 
 class AllDzActivity : Activity() {
@@ -18,38 +19,39 @@ class AllDzActivity : Activity() {
         setContentView(R.layout.activity_alldz)
 
         startAllDz()
-
     }
 
-    private fun startAllDz(){
-        val onClickListener = View.OnClickListener{v->
-            lateinit var intent : Intent
+    private fun startAllDz() {
+        val onClickListener = View.OnClickListener { v ->
+            lateinit var intent: Intent
 
-            when (v.id){
-                R.id.dz0Button ->{
+            when (v.id) {
+                R.id.dz0Button -> {
                     intent = Intent(this, Dz0Activity::class.java)
                 }
-                R.id.dz1Button ->{
+                R.id.dz1Button -> {
                     intent = Intent(this, Dz1Activity::class.java)
                 }
-                R.id.dz2Button ->{
+                R.id.dz2Button -> {
                     intent = Intent(this, Dz2Activity::class.java)
                 }
-                R.id.dz2_2Button ->{
+                R.id.dz2_2Button -> {
                     intent = Intent(this, Dz2LoginActivity::class.java)
                 }
-                R.id.dz3Button ->{
+                R.id.dz3Button -> {
                     intent = Intent(this, Dz3Activity::class.java)
+                }
+                R.id.dz6Button -> {
+                    intent = Intent(this, Dz6StudentListActivity::class.java)
                 }
             }
             startActivity(intent)
-
         }
         dz0Button.setOnClickListener(onClickListener)
         dz1Button.setOnClickListener(onClickListener)
         dz2Button.setOnClickListener(onClickListener)
         dz2_2Button.setOnClickListener(onClickListener)
         dz3Button.setOnClickListener(onClickListener)
-
+        dz6Button.setOnClickListener(onClickListener)
     }
 }
