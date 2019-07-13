3677b38829f3a59c103471f9796a7d5737c7cdff
Maxim Shadchnev <shad88max@gmail.com>
2019-07-13 14:01:46 +0300

    справил текущие замечания

--- app/src/main/java/by/itacademy/myapp/AllDzActivity.kt
+++ app/src/main/java/by/itacademy/myapp/AllDzActivity.kt
@@ -37,10 +37,10 @@ class AllDzActivity : Activity() {
                 R.id.dz1Button -> {
                     intent = Intent(this, Dz1Activity::class.java)
                 }
-                R.id.dz2Button -> {
+                R.id.dz2part1Button -> {
                     intent = Intent(this, Dz2Activity::class.java)
                 }
-                R.id.dz2_2Button -> {
+                R.id.dz2part2Button -> {
                     intent = Intent(this, Dz2LoginActivity::class.java)
                 }
                 R.id.dz3Button -> {
@@ -49,16 +49,16 @@ class AllDzActivity : Activity() {
                 R.id.dz4Button -> {
                     intent = Intent(this, Dz4Activity::class.java)
                 }
-                R.id.dz5_1Button -> {
+                R.id.dz5part1Button -> {
                     intent = Intent(this, Dz5ActivityOwl::class.java)
                 }
-                R.id.dz5_2Button -> {
+                R.id.dz5part2Button -> {
                     intent = Intent(this, Dz5DiagramActivity::class.java)
                 }
                 R.id.dz6Button -> {
                     intent = Intent(this, Dz6StudentListActivity::class.java)
                 }
-                R.id.dz8_button -> {
+                R.id.dz8button -> {
                     intent = Intent(this, Dz8FragmManagerActivity::class.java)
                 }
             }
@@ -66,13 +66,13 @@ class AllDzActivity : Activity() {
         }
         dz0Button.setOnClickListener(onClickListener)
         dz1Button.setOnClickListener(onClickListener)
-        dz2Button.setOnClickListener(onClickListener)
-        dz2_2Button.setOnClickListener(onClickListener)
+        dz2part1Button.setOnClickListener(onClickListener)
+        dz2part2Button.setOnClickListener(onClickListener)
         dz3Button.setOnClickListener(onClickListener)
         dz4Button.setOnClickListener(onClickListener)
-        dz5_1Button.setOnClickListener(onClickListener)
-        dz5_2Button.setOnClickListener(onClickListener)
+        dz5part1Button.setOnClickListener(onClickListener)
+        dz5part2Button.setOnClickListener(onClickListener)
         dz6Button.setOnClickListener(onClickListener)
-        dz8_button.setOnClickListener(onClickListener)
+        dz8button.setOnClickListener(onClickListener)
     }
 }
