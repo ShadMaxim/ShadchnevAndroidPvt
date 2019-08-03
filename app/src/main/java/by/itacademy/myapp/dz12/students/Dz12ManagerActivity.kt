be727bdd0ad7c4d1108ad94835be805da6949b91
ShadMaxim <shad88max@gmail.com>
2019-08-03 14:23:35 +0300

    переношу удаленные файлы

---
+++ app/src/main/java/by/itacademy/myapp/dz12/students/Dz12ManagerActivity.kt
@@ -0,0 +1,96 @@
+package by.itacademy.myapp.dz12.students
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import by.itacademy.myapp.R
+import by.itacademy.myapp.dz12.student.details.Dz12DetailsFragment
+import by.itacademy.myapp.dz12.student.edit.Dz12EditFragment
+import by.itacademy.myapp.dz12.student.list.Dz12ListFragment
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class Dz12ManagerActivity : FragmentActivity(),
+    Dz12ListFragment.Listener,
+    Dz12MyListener {
+
+    private var isLandOrientation = false
+    private var container: Int = 0
+
+    var observable3: Observable<Long> = Observable.interval(1, TimeUnit.SECONDS)
+    var disposable: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz8_for_fragments)
+
+        disposable = observable3
+            .filter { it % 2 == 0.toLong() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                // textView.text = it.toString()
+                Toast.makeText(applicationContext, it.toString(), Toast.LENGTH_SHORT).show()
+            }, {
+                // textView.text = it.toString()
+            })
+
+        isLandOrientation = (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        container = getLandOrPortrait()
+
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.dz8ContainerHead, Dz12ListFragment())
+            transaction.commit()
+        }
+    }
+
+    override fun onStudentClick(id: String) {
+        replaceFragment(getLandOrPortrait(), Dz12DetailsFragment.getInstance(id))
+    }
+
+    override fun onAddButtonClick() {
+        replaceFragment(getLandOrPortrait(), Dz12EditFragment.getInstance())
+    }
+
+    private fun replaceFragment(idContainer: Int, nameFragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(idContainer, nameFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun onRealization() {
+
+        supportFragmentManager.popBackStack()
+        if (isLandOrientation) {
+            (supportFragmentManager.findFragmentById(R.id.dz8ContainerHead) as Dz12ListFragment).updateRecyclerList()
+        } else {
+            replaceFragment(R.id.dz8ContainerHead, Dz12ListFragment())
+        }
+    }
+
+    override fun onEdit(id: String) {
+        if (isLandOrientation) {
+            supportFragmentManager.popBackStack()
+        }
+        replaceFragment(getLandOrPortrait(), Dz12EditFragment.getInstance(id))
+    }
+
+    override fun onIfNullToBack() {
+        supportFragmentManager.popBackStack()
+    }
+
+    private fun getLandOrPortrait(): Int {
+        return if (isLandOrientation) {
+            R.id.dz8ContainerSecond
+        } else {
+            R.id.dz8ContainerHead
+        }
+    }
+}
---
+++ app/src/main/java/by/itacademy/myapp/dz12/students/Dz12MyListener.kt
@@ -0,0 +1,7 @@
+package by.itacademy.myapp.dz12.students
+
+interface Dz12MyListener {
+    fun onRealization()
+    fun onEdit(id: String)
+    fun onIfNullToBack()
+}
